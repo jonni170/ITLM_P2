bc0f499cb5078446a37a83e001e7ed3b80b66117
Kiddi <kiddinn95@gmail.com>
2017-11-03 20:51:24 +0000

    mlemle

--- flappy_agent_q_learning_freedom.py
+++ flappy_agent_q_learning_freedom.py
@@ -9,12 +9,15 @@ class FlappyAgent:
     def __init__(self):
         # TODO: you may need to do some initialization for your agent here
         self.q = {}
-        self.alpha = 0.1
-        self.discount = 0.5
+        self.alpha = 0.2
+        self.discount = 0.1
         self.frameCounter = 0
         self.results = []
         self.ploty = []
         self.plotx = []
+        self.highestScore = 0
+        self.averageScore = 0
+        self.totalScore = 0
         return
 
     def reward_values(self):
@@ -29,10 +32,10 @@ class FlappyAgent:
 
     def discretise(self, state):
         return (
-            int(state['player_y']/(512/15)),
+            int(state['player_y']/(450/15)),
             int(state['player_vel']/(19/15)),
-            int(state['next_pipe_top_y']/(512/15)),
-            int(state['next_pipe_dist_to_player']/(288/15))
+            int(state['next_pipe_top_y']/(250/15)),
+            int(state['next_pipe_dist_to_player']/(280/15))
         )
 
     def observe(self, s1, a, r, s2, end):
@@ -54,8 +57,8 @@ class FlappyAgent:
         b = 1
         if a == 1:
             b = 0
-        qval0 = (self.q[s1][a]*(1-self.alpha)) + (self.alpha*(r + max(self.q[s2][0],self.q[s2][1])))
-        qval1 = (self.q[s1][b]*(1-self.alpha))
+        qval0 = (self.q[s1][a]*(1-self.alpha)) + (self.alpha*(r + self.discount*max(self.q[s2][0],self.q[s2][1])))
+        qval1 = (self.q[s1][b])
         self.q.update({s1: {a: qval0, b: qval1}})
         return
 
@@ -107,45 +110,37 @@ def train_game(nb_episodes, agent):
     """ Runs nb_episodes episodes of the game with agent picking the moves.
         An episode of FlappyBird ends with the bird crashing into a pipe or going off screen.
     """
-
-    # reward_values = {"positive": 1.0, "negative": 0.0, "tick": 0.0, "loss": 0.0, "win": 0.0}
     reward_values = agent.reward_values()
 
     env = PLE(FlappyBird(), fps=30, display_screen=False, force_fps=True, rng=None,
               reward_values=reward_values)
-    # display_screen=False, force_fps=True
     env.init()
 
     score = 0
     while nb_episodes > 0:
         # pick an action
-        # TODO: for training using agent.training_policy instead
         previousState = env.game.getGameState()
         action = agent.training_policy(previousState)
         # step the environment
         reward = env.act(env.getActionSet()[action])
-        # TODO: for training let the agent observe the current state transition
         agent.observe(previousState, action, reward, env.game.getGameState(), env.game_over())
 
         score += reward
         agent.results.append(score)
         agent.frameCounter += 1
-        if agent.frameCounter % 5000 == 0 and agent.frameCounter != 0:
+        if agent.frameCounter % 10000 == 0 and agent.frameCounter != 0:
             sum = 0
             for x in range(len(agent.results)):
                 sum += agent.results[x]
-            avg = sum / 5000
+            avg = sum / 10000
             agent.ploty.append(avg)
             agent.plotx.append(agent.frameCounter)
             agent.results = []
         # reset the environment if the game is over
         if env.game_over():
-            if score > -5:
-                print(score, " ", nb_episodes)
             env.reset_game()
             nb_episodes -= 1
             score = 0
-
 
 def run_game(nb_episodes, agent):
     """ Runs nb_episodes episodes of the game with agent picking the moves.
@@ -153,9 +148,8 @@ def run_game(nb_episodes, agent):
     """
     reward_values = {"positive": 1.0, "negative": 0.0, "tick": 0.0, "loss": 0.0, "win": 0.0}
 
-    env = PLE(FlappyBird(), fps=30, display_screen=False, force_fps=True, rng=None,
+    env = PLE(FlappyBird(), fps=30, display_screen=True, force_fps=False, rng=None,
               reward_values=reward_values)
-
     env.init()
 
     score = 0
@@ -172,9 +166,16 @@ def run_game(nb_episodes, agent):
             print(score, nb_episodes)
             env.reset_game()
             nb_episodes -= 1
+            if score > agent.highestScore:
+                agent.highestScore = score
+            agent.totalScore += score
             score = 0
 
 agent = FlappyAgent()
-train_game(40000, agent)
-run_game(100, agent)
+for x in range (30):
+    train_game(250, agent)
+    agent.alpha -= agent.alpha/4
+    agent.discount += (1-agent.discount)/2
+    print(agent.alpha)
+run_game(50, agent)
 plt.show(sns.barplot(x=agent.plotx, y=agent.ploty))
